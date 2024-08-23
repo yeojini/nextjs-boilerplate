19fe3d215a51b528fd0f149ffb59ae536d46e3e0
yeojini <yesjin1209@gmail.com>
2024-08-23 14:21:29 +0900

    chore: prettier 설치

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,27 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'build', 'ci', 'perf', 'revert']],
-    'scope-enum': [2, 'always', ['app', 'api', 'config', 'docs', 'tests']],
-    'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72],
-    'body-leading-blank': [2, 'always'],
-    'footer-leading-blank': [2, 'always']
-  }
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "chore",
+        "build",
+        "ci",
+        "perf",
+        "revert",
+      ],
+    ],
+    "scope-enum": [2, "always", ["app", "api", "config", "docs", "tests"]],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 72],
+    "body-leading-blank": [2, "always"],
+    "footer-leading-blank": [2, "always"],
+  },
 };
